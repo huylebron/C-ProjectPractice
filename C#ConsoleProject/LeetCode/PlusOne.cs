e02c03cb82eb29d35fc8a84f1d82f3f73f636ec8
huylebron <doquochuy27102003@gmail.com>
2025-04-15 10:49:42 +0700

    Add project files.

---
+++ C#ConsoleProject/LeetCode/PlusOne.cs
@@ -0,0 +1,34 @@
+﻿namespace C_ConsoleProject.LeetCode;
+
+public class PlusOne
+{
+    public int[] Plusone ( int[] digits )
+    {
+        for (var i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        var result = new int[digits.Length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+
+    public static void Main ( string[] args )
+    {
+        var plusOne = new PlusOne ( );
+        int[] digits = { 1, 2, 3 };
+        var result = plusOne.Plusone ( digits );
+
+        Console.WriteLine ( $"Result: {string.Join ( ", ", result )}" );
+        Console.ReadKey ( );
+    }
+}
